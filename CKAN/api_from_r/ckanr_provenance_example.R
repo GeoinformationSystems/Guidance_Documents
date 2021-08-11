2d34f0b9685ba25e5af3b3d70cf90681abdebb8d
Arne Ruemmler <arne.ruemmler@tu-dresden.de>
2021-08-11 16:34:38 +0200

    fix

--- CKAN/api_from_r/ckanr_provenance_example.R
+++ CKAN/api_from_r/ckanr_provenance_example.R
@@ -1,10 +1,13 @@
-# --- To run this example you need to be registered as editor of an 
-# organization in the GeoKur CKAN. 
+# --- To run this example you need to be registered as editor of an
+# organization in the GeoKur CKAN.
 
 # Load required packages
-if (!require("ckanr")) install.packages("ckanr");library ("ckanr")
-if (!require("raster")) install.packages("raster");library ("raster")
-if (!require("rgdal")) install.packages("rgdal");library ("rgdal")
+if (!require("ckanr")) install.packages("ckanr")
+library("ckanr")
+if (!require("raster")) install.packages("raster")
+library("raster")
+if (!require("rgdal")) install.packages("rgdal")
+library("rgdal")
 
 # Configure CKAN connection
 ckanr_setup("https://geokur-dmp.geo.tu-dresden.de/", key = "***")
@@ -21,7 +24,7 @@ ckan_available_datasets <- package_list()
 
 # access the metadata of a given dataset by its "name"
 # package_show returns an R List object
-pollination_metadata <- package_show("demand-and-supply-of-pollination-in-the-european-union-test") 
+pollination_metadata <- package_show("demand-and-supply-of-pollination-in-the-european-union-test")
 
 # view specific metadata fields
 pollination_metadata$id
@@ -31,16 +34,16 @@ pollination_metadata$name
 str(pollination_metadata)
 
 # ignore resource list in view
-str(pollination_metadata, max.level=1)
+str(pollination_metadata, max.level = 1)
 
 # view attached resources
 str(pollination_metadata$resource)
 
 # view name, download url and id of each attached resource (only 1 resource in this case)
-for (resource in pollination_metadata$resource){
-    str(resource$name)
-    str(resource$url)
-    str(resource$id)
+for (resource in pollination_metadata$resource) {
+  str(resource$name)
+  str(resource$url)
+  str(resource$id)
 }
 
 # get resource download url (target resource is nb. 1 in resource list)
@@ -56,11 +59,11 @@ download_url_pollination_ <- resource_show(id = "eeecaa00-4da4-4022-9ae6-8a7849e
 pollination <- raster(download_url_pollination)
 
 # reproject the raster to WGS84
-pollination_reprojected <- projectRaster(pollination, crs="+proj=longlat +datum=WGS84 +no_defs")
+pollination_reprojected <- projectRaster(pollination, crs = "+proj=longlat +datum=WGS84 +no_defs")
 
-# create new Metadata DS in CKAN and store it in an R object (dataset_pollination_proj).
+# create new Metadata DS in CKAN and store it in an R object (dataset_reprojected_metadata).
 
-# !! owner_org and contact_name are mandatory; fill with your name and your organizations 
+# !! owner_org and contact_name are mandatory; fill with your name and your organizations
 # name//id. Get all registered organizations and their names//ids with organization_list()
 pollination_reprojected_metadata <- package_create(
   extras = c(
@@ -79,7 +82,7 @@ pollination_reprojected_metadata <- package_create(
 yield_rapeseed_metadata <- package_show("rapeseed-yield")
 yield_rapeseed <- raster(yield_rapeseed_metadata$resource[[1]]$url)
 
-pollination_resampled <- resample(pollination_reprojected,yield_rapeseed) 
+pollination_resampled <- resample(pollination_reprojected, yield_rapeseed)
 pollination_resampled_metadata <- package_create(
   extras = c(
     type = "dataset",
@@ -89,9 +92,9 @@ pollination_resampled_metadata <- package_create(
     owner_org = "...",
     contact_name = "...",
     was_derived_from = paste(
-      paste0(dataset_base_url, pollination_reprojected_metadata$id), 
-      paste0(dataset_base_url, yield_rapeseed_metadata$id), 
-      sep=","
+      paste0(dataset_base_url, pollination_reprojected_metadata$id),
+      paste0(dataset_base_url, yield_rapeseed_metadata$id),
+      sep = ","
     )
   )
 )
@@ -122,7 +125,8 @@ resample_metadata <- package_create(
     used = paste(
       paste0(dataset_base_url, yield_rapeseed_metadata$id),
       paste0(dataset_base_url, pollination_reprojected_metadata$id),
-      sep=","),
+      sep = ","
+    ),
     generated = paste0(dataset_base_url, pollination_resampled_metadata$id),
     category = "geokur:Transformation"
   )
@@ -131,10 +135,10 @@ resample_metadata <- package_create(
 # ----------------------------------------------------------------------------
 
 # combine pollination and yield data  to table
-output_table <- cbind(as.data.frame(yield_rapeseed),as.data.frame(pollination_resampled)) 
-names(output_table) <- c("yieldRapeseed","pollination")
+output_table <- cbind(as.data.frame(yield_rapeseed), as.data.frame(pollination_resampled))
+names(output_table) <- c("yieldRapeseed", "pollination")
 # remove 0 yields and NAs
-output_table_final <- output_table[which(output_table$yield_rapeseed>0&!is.na(output_table$pollination)),] 
+output_table_final <- output_table[which(output_table$yield_rapeseed > 0 & !is.na(output_table$pollination)), ]
 
 final_dataset_metadata <- package_create(
   extras = c(
@@ -144,7 +148,8 @@ final_dataset_metadata <- package_create(
     notes = "Combined rapeseed yield and pollination as CSV",
     owner_org = "...",
     contact_name = "..."
-))
+  )
+)
 
 cbind_metadata <- package_create(
   extras = c(
@@ -157,7 +162,8 @@ cbind_metadata <- package_create(
     used = paste(
       paste0(dataset_base_url, yield_rapeseed_metadata$id),
       paste0(dataset_base_url, pollination_resampled_metadata$id),
-      sep=","),
+      sep = ","
+    ),
     generated = paste0(dataset_base_url, final_dataset_metadata$id),
     category = "geokur:Selection"
   )
@@ -178,12 +184,14 @@ workflow_metadata <- package_create(
       paste0(dataset_base_url, pollination_metadata$id),
       paste0(dataset_base_url, pollination_resampled_metadata$id),
       paste0(dataset_base_url, pollination_reprojected_metadata$id),
-      sep=","),
+      sep = ","
+    ),
     rel_processes = paste(
       paste0(process_base_url, reproject_metadata$id),
       paste0(process_base_url, resample_metadata$id),
       paste0(process_base_url, cbind_metadata$id),
-      sep=","),
+      sep = ","
+    ),
     result = paste0(dataset_base_url, final_dataset_metadata$id)
   )
 )
@@ -191,10 +199,10 @@ workflow_metadata <- package_create(
 # ----------------------------------------------------------------------------
 
 # add crs entry to list
-pollination_resampled_metadata$conforms_to = "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
+pollination_resampled_metadata$conforms_to <- "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
 
 # remove some CKAN default fields that are not in our scheme and cause errors
-pollination_resampled_metadata[which(names(pollination_resampled_metadata)%in%c("author_email", "maintainer_email"))] <- NULL
+pollination_resampled_metadata[which(names(pollination_resampled_metadata) %in% c("author_email", "maintainer_email"))] <- NULL
 
 # pass all metadata as list(->unclass) to package_update()
 package_update(unclass(pollination_resampled_metadata), pollination_resampled_metadata$id)
@@ -205,11 +213,12 @@ package_update(unclass(pollination_resampled_metadata), pollination_resampled_me
 write.csv(output_table_final, "result.csv")
 
 # upload resource
-resource_metadata <- resource_create(package_id = final_dataset_metadata$id,
-                        name = "rapeseed_and_poll_resource",
-                        upload = "./result.csv",
-                        rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
- )
+resource_metadata <- resource_create(
+  package_id = final_dataset_metadata$id,
+  name = "rapeseed_and_poll_resource",
+  upload = "./result.csv",
+  rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+)
 
 # ----------------------------------------------------------------------------
 
